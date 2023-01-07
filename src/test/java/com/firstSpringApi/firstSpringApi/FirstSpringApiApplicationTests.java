12ed184989de1f0bd979f50058c8b16f5a545d60
DanielMuñoz <joseda942209@gmail.com>
2023-01-07 08:57:30 -0500

    init project

---
+++ src/test/java/com/firstSpringApi/firstSpringApi/FirstSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstSpringApi.firstSpringApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
